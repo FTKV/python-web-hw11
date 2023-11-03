dd9c15e230b0c094598407b177ba0266a3c68413
FTKV <mthkvv@gmail.com>
2023-11-03 04:58:42 +0000

    fix aiohttp

--- seed.py
+++ seed.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import platform
 
 import aiohttp
 import faker
@@ -11,7 +12,7 @@ NUMBER_CONTACTS = 1000
 fake_data = faker.Faker("uk_UA")
 
 
-async def get_fake_contact():
+async def get_fake_contacts():
     for _ in range(NUMBER_CONTACTS):
         yield (lambda x: [x[1], x[2]] if len(x) == 3 else [x[0], x[1]])(
             fake_data.name().split(" ")
@@ -26,7 +27,7 @@ async def get_fake_contact():
 async def send_data_to_api() -> None:
     headers = {"content-type": "application/json"}
     session = aiohttp.ClientSession()
-    async for first_name, last_name, email, phone, birthday, address in get_fake_contact():
+    async for first_name, last_name, email, phone, birthday, address in get_fake_contacts():
         data = {
             "first_name": first_name,
             "last_name": last_name,
@@ -35,13 +36,19 @@ async def send_data_to_api() -> None:
             "birthday": birthday,
             "address": address,
         }
-
-        await session.post(
-            "http://127.0.0.1:8000/api/contacts", data=json.dumps(data), headers=headers
-        )
+        try:
+            await session.post(
+                "http://127.0.0.1:8000/api/contacts",
+                data=json.dumps(data),
+                headers=headers,
+            )
+        except aiohttp.ClientOSError as err:
+            print(f"Connection error: {str(err)}")
     await session.close()
     print("Done")
 
 
 if __name__ == "__main__":
+    if platform.system() == "Windows":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(send_data_to_api())
